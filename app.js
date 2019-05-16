d60618c29af56bb4321dd38a7a4fd230c64a2f38
Broadwater G <frankj1995@hotmail.co.uk>
2019-05-16 22:11:47 +0100

    Add delete button, and delete popup asking for admin credentials

--- app.js
+++ app.js
@@ -48,8 +48,21 @@ const passwordSchema = new mongoose.Schema({
   }
 });
 
+const cardSchema = {
+  card_provider: String,
+  account_holder: String,
+  card_number: String,
+  expiry_date: String,
+  security_code: {
+    type: String,
+    required: true
+  },
+  pin: String
+}
+
 const Password = mongoose.model('password', passwordSchema);
 const Admin = mongoose.model('admin', adminScehma);
+const Card = mongoose.model('card', cardSchema);
 
 function encryptPassword(username, password, saltRounds) {
   Admin.find(function(err, admin) {
@@ -72,15 +85,13 @@ app.get('/', function(req, res) {
 });
 
 app.post('/login', function(req, res) {
-  Admin.findOne({username: req.body.username}, function(err, admin) {
-    if(admin) {
+  Admin.findOne({
+    username: req.body.username
+  }, function(err, admin) {
+    if (admin) {
       bcrypt.compare(req.body.password, admin.password, function(err, result) {
-        if(result === true) {
-          Password.find(function(err, passwords) {
-            if(!err) {
-              res.render('passwords');
-            }
-          });
+        if (result === true) {
+          res.render('passwords');
         } else {
           res.send('Incorrect password');
         }
@@ -91,16 +102,66 @@ app.post('/login', function(req, res) {
   });
 });
 
-
-
 app.get('/passwords', function(req, res) {
   res.render('passwords');
 });
 
-app.get('/creditcards', function(req, res) {
-  res.render('creditcards');
+app.route('/creditcards')
+  .get(function(req, res) {
+    Card.find(function(err, card) {
+      if (!err) {
+        if (card) {
+          res.render('creditcards', {
+            cardItems: card
+          });
+        } else {
+          res.render('creditcards');
+        }
+      } else {
+        console.log(err);
+      }
+    });
+  })
+  .post(function(req, res) {
+
+    const newCard = new Card({
+      card_provider: req.body.card_provider,
+      account_holder: req.body.account_holder,
+      card_number: req.body.card_number,
+      expiry_date: req.body.expiry_date,
+      security_code: req.body.security_code,
+      pin: req.body.pin
+    })
+    newCard.save();
+    res.redirect('/creditcards');
+  });
+
+app.post('/ccdelete', function(req, res) {
+  Admin.findOne({
+    username: req.body.username
+  }, function(err, admin) {
+    if (admin) {
+      bcrypt.compare(req.body.password, admin.password, function(err, result) {
+        if (result === true) {
+          Card.deleteOne({
+            security_code: req.body.identifier
+          }, function(err) {
+            if (!err) {
+              res.redirect('/creditcards');
+            } else {
+              res.send('Please enter valid security number');
+            }
+          });
+        } else {
+          res.send('Please enter correct password');
+        }
+      });
+    } else {
+      res.send('Please enter correct admin username');
+    }
+  });
 });
 
 app.listen(3000, function() {
-  console.log('Server connected on port 3000.');
+  console.log('Listening on port 3000');
 });
--- public/index.js
+++ public/index.js
@@ -1,5 +1,12 @@
-document.getElementById("item-1").addEventListener("click", clicked);
+var deleteBtn = document.getElementsByClassName('delete-btn');
 
-function clicked() {
-
+for (i = 0; i < deleteBtn.length; i++) {
+  deleteBtn[i].addEventListener('click', function(e) {
+    document.querySelector('.cc-modal').style.display = 'flex';
+  });
 }
+
+document.querySelector('.close').addEventListener('click', function() {
+
+  document.querySelector('.cc-modal').style.display = 'none';
+});
